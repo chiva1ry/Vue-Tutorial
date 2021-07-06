ca4a19339e8fb401f298c78b1ba85677905034a9
SamJin <sam_ruilin@hotmail.com>
2021-07-06 13:27:43 +0800

    webpack1

---
+++ Web/01-webpack的使用/01-webpack的起步/src/main.js
@@ -0,0 +1,9 @@
+const { add, mult } = require('/mathUtils.js')
+
+console.log(add(20, 30));
+console.log(mult(20, 30));
+
+import { name, age, height } from './info';
+console.log(name);
+console.log(age);
+console.log(height)
